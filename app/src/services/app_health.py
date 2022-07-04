9b0857d2549ee831bf926ff5c94022b15ed44fa8
lihilu <lihi.reisman@gmail.com>
2022-07-04 12:58:20 +0300

    changed script with anothe module

--- app/src/services/app_health.py
+++ app/src/services/app_health.py
@@ -2,9 +2,8 @@ from boto3 import client
 from botocore.exceptions import ClientError
 import json
 import botocore 
-import botocore.session 
-from aws_secretsmanager_caching import SecretCache, SecretCacheConfig
 import psycopg2
+import base64
 
 AWS_REGION="us-east-1"
 ec2_client = client('ec2', region_name=AWS_REGION)
@@ -21,28 +20,59 @@ def check_aws_connection():
         return False
 
 
-def check_db_connection():
-
+def aws_secret_manager(secretid):
     client = botocore.session.get_session().create_client('secretsmanager')
-    cache_config = SecretCacheConfig()
-    cache = SecretCache( config = cache_config, client = client)
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secretid)
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'DecryptionFailureException':
+            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
+            # An error occurred on the server side.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidParameterException':
+            # You provided an invalid value for a parameter.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidRequestException':
+            # You provided a parameter value that is not valid for the current state of the resource.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+            # We can't find the resource that you asked for.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+    else:
+        # Decrypts secret using the associated KMS CMK.
+        # Depending on whether the secret is a string or binary, one of these fields will be populated.
+        if 'SecretString' in get_secret_value_response:
+            secret = get_secret_value_response['SecretString']
+        else:
+            secret = base64.b64decode(get_secret_value_response['SecretBinary'])
 
-    secret = cache.get_secret_string('kanduladblihi')
-    secret_json=json.loads(secret)
+    secret_load= json.loads(secret)  # returns the secret as dictionary
+    # print (secret_load)
+    # db_user=  secret_load['username']
+    # db_password=  secret_load['password']
+    # db_host = secret_load['host']
+    # db_name = secret_load['dbname']
+    # print (db_user, db_password,db_host, db_name)
+    return (secret_load)
 
-    db_user=secret_json['username']
-    db_pass=secret_json['password']
-    db_name= secret_json['dbname']
-
-    connection = psycopg2.connect(
-    host = 'your_RDB_AWS_instance_Endpoint',
-    port = 5432,
-    user = 'YOUR_USER_NAME',
-    password = 'YOUR_PASSWORD',
-    database='YOUR_DATABASE_NAME'
-    )
-    cursor=connection.cursor()
-    print ("hfkjdshflkdsahfdksjhfaksjdhf" + cursor)
+def check_db_connection():
+    db_info=  aws_secret_manager('kanduladblihi')
+    print (db_info['username'])
+    conn = psycopg2.connect(database=db_info['dbname'],
+                        host=db_info['host'],
+                        user=db_info['username'],
+                        password=db_info['password'],
+                        port=5432)
+    
+    print (conn)
     return True
 
 
