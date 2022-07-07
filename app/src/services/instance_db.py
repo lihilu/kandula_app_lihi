64e2d5c34dd3082d8c4d9640b129ac7d464f0477
lihilu <lihi.reisman@gmail.com>
2022-07-07 11:21:26 +0300

    finally changed

--- app/src/services/instance_db.py
+++ app/src/services/instance_db.py
@@ -39,9 +39,11 @@ def get_scheduling():
         print("Error fetching data from PostgreSQL table", error)
 
     finally:
-        # closing database connection
-        cur.close()
-        print("PostgreSQL connection is closed \n")
+    # closing database connection.
+        if conn:
+            cur.close()
+            conn.close()
+            print("PostgreSQL connection is closed")
     return instance_schedule
 
 
@@ -74,7 +76,7 @@ def create_scheduling(instance_id, shutdown_hour):
         conn.commit()
         count = cursor.rowcount
         print(count, "Record inserted successfully into mobile table")       
-        
+
         index = [i['Id'] for i in instance_schedule["Instances"]].index(instance_id)
         instance_schedule["Instances"][index] = {"Id": instance_id, "DailyShutdownHour": int(shutdown_hour[0:2])}
         print("Instance {} will be shutdown was updated to the hour {}".format(instance_id, shutdown_hour))
@@ -82,6 +84,12 @@ def create_scheduling(instance_id, shutdown_hour):
         instance_schedule["Instances"].append({"Id": instance_id, "DailyShutdownHour": int(shutdown_hour[0:2])})
         print("Instance {} will be shutdown every day when the hour is {}".format(instance_id, shutdown_hour))
 
+    finally:
+    # closing database connection.
+        if conn:
+            cursor.close()
+            conn.close()
+            print("PostgreSQL connection is closed")
 
 def delete_scheduling(instance_id):
     # TODO: Implement a delete query to remove the instance ID from scheduling
