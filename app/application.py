b6ba909b82cc01cc82ba0da92bb31d213a63a02d
lihilu <lihi.reisman@gmail.com>
2022-07-29 17:11:31 +0300

    test

--- app/application.py
+++ app/application.py
@@ -13,7 +13,7 @@ from prometheus_flask_exporter import PrometheusMetrics
 
 
 def create_app() -> Flask:
-    PrometheusMetrics(app)
+  
     container = Container()
     container.config.from_yaml('config.yml')
     container.config.aws_access_key_id.from_env('AWS_ACCESS_KEY_ID')
@@ -53,7 +53,8 @@ def create_app() -> Flask:
 
     bootstrap = Bootstrap()
     bootstrap.init_app(app)
-
+    PrometheusMetrics(app)
     return app
 
+    
    
