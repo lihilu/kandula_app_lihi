6f1423c9e225bbd10840e8c2504c48c5faaad537
lihilu <lihi.reisman@gmail.com>
2022-07-30 21:35:24 +0300

    count test 1

--- run.py
+++ run.py
@@ -15,3 +15,11 @@ def validate_mandatory_env_variables():
 if __name__ == "__main__":
     validate_mandatory_env_variables()
     app.run(host='0.0.0.0', use_evalex=False)
+
+count = 0
+@app.route("/count")
+def home():
+    global count
+    print(count)
+    count += 1
+    return "testing"
